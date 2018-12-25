a451bcdd69f5e6fc529b764a7edf1e1c08dd51cd
sstoyanov66 <46057215+sstoyanov66@users.noreply.github.com>
2018-12-25 13:26:20 +0200

    Add files via upload

--- assets/js_gibb/gibbet.js
+++ assets/js_gibb/gibbet.js
@@ -1,68 +1,117 @@
-/**
- * 
- */
-		
-/*This is to Enable/disable the button for entire word*/
-		function enableButton(){
-			   
-			 var wholeWord = document.getElementById("guess").value; 
-			var wholeWords = wholeWord.trim();
-			  
-			  	 			  			  
-			  if(wholeWords!="" && $('#word').html()!=""){			    			    	
-			    	document.getElementById("guessButt").disabled = false;
-			      							    	
-			    }else{
-			    	document.getElementById("guessButt").disabled = true;
-			    }	
+   
+    /*This is to Enable/disable the button for entire word*/
+	function enableButton(){
+		   
+		 var wholeWord = document.getElementById("guess").value; 
+		var wholeWords = wholeWord.trim();
 		  
-			  }//end  enableButton
-						
-									  		  				
+		  	 			  			  
+		  if(wholeWords!="" && $('#word').html()!=""){			    			    	
+		    	document.getElementById("guessButt").disabled = false;
+		      							    	
+		    }else{
+		    	document.getElementById("guessButt").disabled = true;
+		    }	
+	  
+		  }//end  enableButton
+							
+		
 $(document).ready(function(){
-			
+
+	
+    /*This is to Enable/disable the button for entire word
+
+       $('#guess').focus(function () {
+   		 	
+           if ($('#guess').val()!='' && $('#word').html()!="") {
+               $('#guessButt').prop('disabled', false);         
+           } else {
+               $('#guessButt').prop('disabled', true);                       
+           }
+       });
+          */
+    
+    /*disable Enter key if the word is not guessed to prevent new word load w/o warning message and losing game*/
+    $(document).keypress(
+	    function(event){
+	      if ( $('#word').html()!="" && event.which == '13') {
+	        event.preventDefault();
+	      }
+	  });
+    
+					         
 	  /*to click the hidden buttons inside the input form  form outside:*/
-    $('#signIt').on('click', function (e) {        	       	
+    $('#signIt').on('click', function () {        	       	
         $('#hiddensignup').click();
  			    
       	});
     
-    /*to enable/disable start game button*/
-  
+
+    /*to enable/disable start game and new game buttons - only if there's option selected*/
+    
     $('#slctType').focus(function () {
+		 	
         if ($(this).val() == '') {
             $('#startGame').prop('disabled', true);
+            $('#newGame').prop('disabled', true);
         } else {
             $('#startGame').prop('disabled', false);
+            $('#newGame').prop('disabled', false);
+                        
         }
     });
+    
     
     /*to enable/disable Letter buttons*/
    if ($('#word').html()!=""){
            $('.btn-sm').prop('disabled', false);
+                       
       }else{  
     	  $('.btn-sm').prop('disabled', true);
      }
     
-    	
-   /*try to Exit or exit:*/
-   $('#tryExit').on('click', function (e) { 
-	   if ($('#word').html()!=""){
+    /*	 if (this.keyCode== 13 && $('#word').html()!="") { 
+	        $('#warnModal').modal('show'); 
+	       
+	   }*/
+
+   
+   /*try to Exit or exit:*/  
+     
+   $('#tryExit').on('click', function () { 
+         
+	   if ( $('#word').html()!=""){
 	   $('#warnModal').modal('show'); 
 	   }else{
-		   $('#hiddenexit').click();
-	   }
+	      
+		   $('#hiddenexit1').click();
+	   }	       
      	});
    
+   /*to click the hidden buttons inside the input form form outside:*/
+   $('#exitDB').on('click', function () {   
+       
+       $('#hiddenexit2').click();
+			    
+     	});
+      
+   
    /*try to start a new game - check if there's a current one:*/
-   $('#startGame').on('click', function (e) { 
-	   if ($('#word').html()!=""){
+   $('#startGame').on('click', function () { 
+        
+	      if ($('#word').html()!=""){
 		   $('#warnModal').modal('show'); 
 		   }else{
-			   $('#hiddenstart').click();
+			   $('#hiddenstart1').click();
 		   }
-	  	
+	      	 	
    });
+   
+   /*to click the hidden buttons inside the input form form outside:*/
+   $('#newGame').on('click', function () {        	       	
+       $('#hiddenstart2').click();
+			    
+     	});
    
     /*initialize Letter buttons with Cyrillic values*/
    document.getElementById("a").value = "а"; document.getElementById("b").value = "б"; document.getElementById("v").value = "в"; document.getElementById("g").value = "г";
