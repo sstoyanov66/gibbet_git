294970764c2c2bd59e84b0dcbbc5f6ea5e94891f
sstoyanov66 <46057215+sstoyanov66@users.noreply.github.com>
2018-12-25 13:25:42 +0200

    Add files via upload

---
+++ assets/js_gibb/gibbet_enter.js
@@ -0,0 +1,10 @@
+					
+$(document).ready(function(){
+    					         
+	  /*to click the hidden buttons inside the input form  form outside:*/
+    $('#signIt').on('click', function () {        	       	
+        $('#hiddensignup').click();
+ 			    
+      	});
+    
+});// 
